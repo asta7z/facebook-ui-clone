8f27868b40b5a527715ddda1bbb6575f00fd951e
asta7z <oussamaboutaleb1042004@gmail.com>
2025-02-19 17:04:50 +0000

    initial commit

--- src/components/Container/Info.js
+++ src/components/Container/Info.js
@@ -7,7 +7,7 @@ var info = {
     bio: "ur future self is talking shit about u :!",
     user_ig_p: 'asta.7z',
     user_x_p: 'stark7z',
-    user_g_p: 'asta.7z',
+    user_g_p: 'asta7z',
     my_story: images.nature1,
     followers_num: 500,
 }
